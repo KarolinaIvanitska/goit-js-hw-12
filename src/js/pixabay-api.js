d9b52918ccfd2821decf73cfe59610e26eb42207
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-03-12 15:16:34 +0100

    push

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,23 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '42814531-6217cd85928bc5270423ce53d';
 
-export function getPhotos(q) {
-  const params = new URLSearchParams({
+let page = 1;
+let perPage = 15;
+let totalHits = 0;
+const lastPage = Math.ceil(totalHits / perPage);
+
+export async function getPhotos(q) {
+  const params = {
     key: API_KEY,
     q,
+    _page: page,
+    _limit: perPage,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
-  const url = `${BASE_URL}/?${params}`;
-
-  return fetch(url).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
+  };
+  const res = await axios.get(`${BASE_URL}/`, { params });
+  return res.data;
 }
--- src/main.js
+++ src/main.js
@@ -2,19 +2,31 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
 
-import { renderMarkup, refreshLightBox } from './js/render-function';
+import {
+  renderMarkup,
+  refreshLightBox,
+  imageTemplate,
+} from './js/render-function';
 import { getPhotos } from './js/pixabay-api';
 
 const formElem = document.querySelector('.search-form');
 const loader = document.querySelector('.loader');
 const imageEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more-btn');
 
-formElem.addEventListener('submit', e => {
+hideLoadMoreBtn();
+
+formElem.addEventListener('submit', async e => {
   e.preventDefault();
+
   imageEl.innerHTML = '';
+  page = 1;
+  showLoadMoreBtn();
 
   const userSearch = e.target.elements.search.value.trim();
+
   if (userSearch === '') {
     iziToast.warning({
       titleColor: '#fff',
@@ -25,34 +37,31 @@ formElem.addEventListener('submit', e => {
     });
     return;
   }
-
   showLoader();
-
-  getPhotos(userSearch)
-    .then(data => {
-      if (data.hits.length === 0) {
-        return iziToast.error({
-          title: 'Error',
-          titleColor: '#fff',
-          messageColor: '#fff',
-          backgroundColor: '#ef4040',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      }
-      renderMarkup(imageEl, data.hits);
-      refreshLightBox();
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      hideLoader();
-    });
-
+  try {
+    const data = await getPhotos(userSearch, page);
+    if (data.hits.length === 0) {
+      return iziToast.error({
+        title: 'Error',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        backgroundColor: '#ef4040',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    }
+    renderMarkup(imageEl, data.hits);
+    refreshLightBox();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoader();
+  }
   e.target.reset();
 });
+
+//=========LOADER=============================
 
 function showLoader() {
   if (loader) {
@@ -65,3 +74,46 @@ function hideLoader() {
     loader.style.display = 'none';
   }
 }
+
+//============BTN-LOAD-MORE==================
+
+function hideLoadMoreBtn() {
+  if (loadMoreBtn) {
+    loadMoreBtn.style.display = 'none';
+  }
+}
+
+function showLoadMoreBtn() {
+  if (loadMoreBtn) {
+    loadMoreBtn.style.display = 'block';
+  }
+}
+
+//========================================
+let page = 1;
+let perPage = 15;
+let UserSearch;
+let totalHits = 0;
+const lastPage = Math.ceil(totalHits / perPage);
+//========================================
+
+loadMoreBtn.addEventListener('click', async e => {
+  page += 1;
+  const data = await getPhotos(userSearch, page);
+
+  renderMarkup(imageEl, data.hits);
+
+  const lastPage = Math.ceil(totalHits / perPage);
+
+  if (lastPage === page) {
+    showLoadMoreBtn();
+    iziToast.error({
+      title: 'Error',
+      titleColor: '#fff',
+      messageColor: '#fff',
+      backgroundColor: '#ef4040',
+      message: 'We are sorry, but you have reached the end of search results',
+      position: 'topRight',
+    });
+  }
+});
